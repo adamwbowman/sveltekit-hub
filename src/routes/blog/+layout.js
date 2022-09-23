7f591bbfd56ac563b437c9ae946022d80689fdbf
Adam Bowman <adamwbowman@gmail.com>
2022-09-23 09:56:59 -0500

    new navbar color, rough blog framing

--- src/routes/blog/+layout.js
+++ src/routes/blog/+layout.js
@@ -8,10 +8,11 @@ export function load() {
 			{ year: '2022', month: '12', day: '02', title: 'title', text: 'test text here, test text here' },
 			{ year: '2022', month: '12', day: '03', title: 'title', text: 'test text here, test text here' },
 			{ year: '2022', month: '12', day: '04', title: 'title', text: 'test text here, test text here' },
-			{ year: '2022', month: '12', day: '05', title: 'title', text: 'test text here, test text here' },
-			{ year: '2022', month: '12', day: '06', title: 'title', text: 'test text here, test text here' },
-			{ year: '2022', month: '12', day: '07', title: 'title', text: 'test text here, test text here' },
-			{ year: '2022', month: '12', day: '08', title: 'title', text: 'test text here, test text here' },
+		],
+		sections: [
+			{ route: '#', title: 'Link here' },
+			{ route: '#', title: 'Link here' },
+			{ route: '#', title: 'Link here' },
 		]
 	};
 }
--- src/routes/blog/+layout.svelte
+++ src/routes/blog/+layout.svelte
@@ -6,15 +6,20 @@
 	export let data;
 </script>
 
+<nav class="navbar navbar-expand mt-2" style="background-color: #fcecbb;">
+	<div class="container-fluid justify-content-center">
+		<ul class="navbar-nav">
+		{#each data.sections as section}
+			<li class="nav-item py-0">
+				<a class="nav-link py-0" href="/{section.route}"><small>{section.title}</small></a>
+			</li>
+		{/each}
+		</ul>
+	</div>
+</nav>
 
-{#each data.posts as post}
+<!-- {#each data.posts as post}
 	<p>{post.title}: {post.month}/{post.day}/{post.year}</p> &nbsp;
-{/each}
-
-<h3>
-	{$page.params.year}, 
-	{$page.params.month}, 
-	{$page.params.day}
-</h3>
+{/each} -->
 
 <slot />
